3a069de55c5875bc25a89f3b73af7cd1afb5f3b4
Vasundhara Pandey <64275624+vpandey2510@users.noreply.github.com>
2025-01-03 13:59:02 +0530

    Update settings.py

--- Task_Management_System/settings.py
+++ Task_Management_System/settings.py
@@ -179,8 +179,7 @@ CELERY_BEAT_SCHEDULE = {
 }
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'vasundhara.2510@gmail.com'
