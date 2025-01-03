05275c613a1abf1eaa9fa84e57d96f83f1a998a7
Vasundhara Pandey <64275624+vpandey2510@users.noreply.github.com>
2025-01-03 13:57:14 +0530

    Update settings.py

--- Task_Management_System/settings.py
+++ Task_Management_System/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tttn)svq()fgh&6ces*vu!9$pw@o^nw_2z3+8s0%oce#bp)q_8'
+
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
